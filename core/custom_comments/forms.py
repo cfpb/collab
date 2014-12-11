a3bc3d34d48a5cb4f853ed9b8cc41ee41a155ca5
Mike Brown <michael.brown@cfpb.gov>
2014-12-11 19:07:23 +0000

    Add 'anonymous' option to comment form

--- core/custom_comments/forms.py
+++ core/custom_comments/forms.py
@@ -5,6 +5,7 @@ from models import MPTTComment
 
 class MPTTCommentForm(CommentForm):
     parent = forms.ModelChoiceField(queryset=MPTTComment.objects.all(), required=False, widget=forms.HiddenInput)
+    is_anonymous = forms.BooleanField(required=False, label="Hide commenter's name")
 
     def get_comment_model(self):
         # Use our custom comment model instead of the built-in one.
@@ -14,4 +15,5 @@ class MPTTCommentForm(CommentForm):
         # Use the data of the superclass, and add in the parent field field
         data = super(MPTTCommentForm, self).get_comment_create_data()
         data['parent'] = self.cleaned_data['parent']
+        data['is_anonymous'] = self.cleaned_data['is_anonymous']
         return data
--- core/custom_comments/models.py
+++ core/custom_comments/models.py
@@ -1,10 +1,14 @@
 from django.contrib.comments.models import Comment
 from mptt.models import MPTTModel, TreeForeignKey
+from django.db import models
 
 class MPTTComment(MPTTModel, Comment):
     """ Threaded comments - Add support for the parent comment store and MPTT traversal"""
     # a link to comment that is being replied, if one exists
     parent = TreeForeignKey('self', null=True, blank=True, related_name='children')
+
+    is_anonymous = models.BooleanField('is anonymous', default=False,
+                                       help_text='Check this box to keep your name private.')
 
     class MPTTMeta:
         # comments on one level will be ordered by date of creation
