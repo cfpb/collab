7ce03f4ad803731fdb1ec1695d0c71b200b75e91
Jimmy Wilson <jimmynotjim@me.com>
2016-12-22 09:23:28 -0600

    Save the open group when searching (#60)
    Save the open group when searching
    
    When opening a search results group, the group is saved to sessionStorage to prevent it from collapsing when a user returns to their results. When a user initiates a new search, any previously saved open group is removed from the session storage.

--- core/search/templates/search/search_results.js
+++ core/search/templates/search/search_results.js
@@ -1,12 +1,20 @@
 function collapseResults( group ) {
+    var openGroup = sessionStorage.getItem( 'openGroup' );
+    var dataModel = group.attr( 'data-model' );
+
+    if ( dataModel == openGroup ) {
+        return;
+    }
+    
     var results = $( group ).find( 'ol' ).children( 'li' );
-    if ( results.length > 5 ) {
+    if ( results.length > 5 && dataModel !== openGroup ) {
         results.hide().slice( 0, 5 ).show();
         var link = $( '<a class="more_results btn" href="#">' + ( results.length - 5 ) + ' More results</a>' );
         link.click( function( e ) {
             e.preventDefault();
             results.slideDown();
             $( this ).hide();
+            sessionStorage.setItem( 'openGroup', dataModel );
         } );
         $( group ).append( link );
     }
@@ -23,7 +31,6 @@ function collapseResults( group ) {
 
             if ( results.length > 0 ) {
                 wikiCount.html( results.length );
-
                 for ( var count = 0; count < results.length; count++ ) {
                     var link = results[count].title.replace( ' ', '_' );
                     var html = '<li><a href="/wiki/index.php/' + link + '">';
--- core/static/js/global.js
+++ core/static/js/global.js
@@ -1,20 +1,24 @@
-$(document).ready(function() {
-
-    $(".tag").tooltip({
+$( document ).ready( function() {
+    $( '.tag' ).tooltip( {
         delay: 100
-    });
+    } );
 
-    function showErrorOrRedirect(data) {
-        if(data.redirect) {
-            window.location.replace(data.redirect);
+    function showErrorOrRedirect( data ) {
+        if ( data.redirect ) {
+            window.location.replace( data.redirect );
         } else {
-            createAlert(data.error);
+            createAlert( data.error );
         }
     }
 
-    function createAlert(msg) {
-        $("#msg-bar-error").append("<div style='color: red;'>" + msg + "</div>");
+    function createAlert( msg ) {
+        $( '#msg-bar-error' ).append( '<div style="color: red;">' + msg + '</div>' );
     }
 
     window.submitOptions = { success: showErrorOrRedirect };
-});
+    
+    // Clear the open search results group with a new search
+    $( '#greybar-search-form' ).on( 'submit', function() {
+        sessionStorage.removeItem( 'openGroup' );
+    } );
+} );
