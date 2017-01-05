cfpb/collab

commit 18d9658bf84c8721ae8a6980098c9744e91ad20f

Show top suggested result in Wiki results (#61)
Updated the wiki results to showcase a suggested result above the list of results. Does not include any style changes. Does include some cleanup in the original result list scripting.

core/search/templates/search/search_results.js
@@ -6,7 +6,7 @@ function collapseResults( group ) {
         return;
     }
     
-    var results = $( group ).find( 'ol' ).children( 'li' );
+    var results = group.find( 'ol' ).children( 'li' );
     if ( results.length > 5 && dataModel !== openGroup ) {
         results.hide().slice( 0, 5 ).show();
         var link = $( '<a class="more_results btn" href="#">' + ( results.length - 5 ) + ' More results</a>' );
@@ -31,17 +31,27 @@ function collapseResults( group ) {
 
             if ( results.length > 0 ) {
                 wikiCount.html( results.length );
-                for ( var count = 0; count < results.length; count++ ) {
-                    var link = results[count].title.replace( ' ', '_' );
-                    var html = '<li><a href="/wiki/index.php/' + link + '">';
-                    html += results[count].title;
-                    html += '</a> <br/>';
-                    html += results[count].snippet
-                            .replace( /<(\/)?div.*?>/gm, '' )
-                            .replace( /<(\/)?span.*?>/gm, "<$1b>" );
-                    html += '</li>';
-                    wikiList.append( html );
+                
+                var suggestedData = results[0];
+                var suggestedLink = suggestedData.title.replace( ' ', '_' );
+                var suggestedContent = suggestedData.snippet.replace( /<(\/)?div.*?>/gm, '' );
+                var suggestedHTML = '<div class="suggested-result">';
+                    suggestedHTML += '<h4>Suggested result</h4>';
+                    suggestedHTML += '<a href="/wiki/index.php/' + suggestedLink + '">';
+                    suggestedHTML += suggestedData.title + '</a>';
+                    suggestedHTML += '<p>' + suggestedContent + '</p></div>';
+                wikiList.prepend( suggestedHTML );
+
+                for ( var count = 1; count < results.length; count++ ) {
+                    var resultData = results[count];
+                    var resultLink = resultData.title.replace( ' ', '_' );
+                    var resultContent = resultData.snippet.replace( /<(\/)?div.*?>/gm, '' );
+                    var resultHTML = '<li><a href="/wiki/index.php/' + resultLink + '">';
+                        resultHTML += resultData.title + '</a>';
+                        resultHTML += '<p>' + suggestedContent + '</p></li>';
+                    wikiList.append( resultHTML );
                 }
+                
                 collapseResults( wikiGroup );
             } else {
                 wikiGroup.hide();

commit 92a85f783d2818e89ac6f1d4621a090bf1e341f9

bugfix: wiki result content (#63)
Fixes a bug in the wiki result content introduced in #61

core/search/templates/search/search_results.js
@@ -48,7 +48,7 @@ function collapseResults( group ) {
                     var resultContent = resultData.snippet.replace( /<(\/)?div.*?>/gm, '' );
                     var resultHTML = '<li><a href="/wiki/index.php/' + resultLink + '">';
                         resultHTML += resultData.title + '</a>';
-                        resultHTML += '<p>' + suggestedContent + '</p></li>';
+                        resultHTML += '<p>' + resultContent + '</p></li>';
                     wikiList.append( resultHTML );
                 }
                 